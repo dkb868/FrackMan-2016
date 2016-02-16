bed10028646ce57ee39a3e6f278a8eb653fb5932
Dmitri Brereton <mitrikyle@yahoo.com>
2016-02-15 16:42:31 -0800

    Stopped decrementing lives when move

--- StudentWorld.cpp
+++ StudentWorld.cpp
@@ -22,8 +22,7 @@ int StudentWorld::move()
 {
 	// This code is here merely to allow the game to build, run, and terminate after you hit enter a few times.
 	// Notice that the return value GWSTATUS_PLAYER_DIED will cause our framework to end the current level.
-	decLives();
-	return GWSTATUS_PLAYER_DIED;
+	return GWSTATUS_CONTINUE_GAME;
 }
 
 void StudentWorld::cleanUp(){
